52bbb2a7caca2c7af67f9974530cf18c765a8ab7
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-05 11:10:29 -0600

    Remove.

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-import { h, Component } from 'preact';
-
-const Home = () => {
-   return (
-      <h1>Home Page</h1>
-   )
-}
-
-export default Home;
--- routes/signup.js
+++
@@ -1,9 +0,0 @@
-import { h, Component } from 'preact';
-
-const Signup = () => {
-   return (
-      <h1>Signup Page</h1>
-   )
-}
-
-export default Signup;
