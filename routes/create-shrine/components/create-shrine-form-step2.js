d07d212ea6b353b7e208492b3d817ac3218bb920
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-20 21:21:41 -0600

    Don't forget props and buttons

--- routes/create-shrine/components/create-shrine-form-step2.js
+++ routes/create-shrine/components/create-shrine-form-step2.js
@@ -1,14 +1,19 @@
 import { h, Component } from 'preact';
 
 import { FileInput } from '../../../components/form-inputs/';
+import { NextStepButton } from './next-step-button';
 
-const CreateShrineFormStep2 = () => {
+const CreateShrineFormStep2 = (props) => {
    return (
       <div>
          <h1>Create a New Shrine</h1>
          <h4>Step 2</h4>
          <FileInput 
+            class="text-ellipsis"
             label="Upload a Photo" 
+         />
+         <NextStepButton 
+            onClick={ props.handleNextStep } 
          />
       </div>
    );
