0c27a6197f2a69d0b111d8ac59b438332e8cc1b7
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-20 21:16:58 -0600

    Update container with all step templates

--- routes/create-shrine/components/create-shrine-form-container.js
+++ routes/create-shrine/components/create-shrine-form-container.js
@@ -3,6 +3,10 @@ import { route } from 'preact-router';
 
 import { CreateShrineFormStep1 } from './create-shrine-form-step1';
 import { CreateShrineFormStep2 } from './create-shrine-form-step2';
+import { CreateShrineFormStep3 } from './create-shrine-form-step3';
+import { CreateShrineFormStep4 } from './create-shrine-form-step4';
+import { CreateShrineFormStep5 } from './create-shrine-form-step5';
+import { CreateShrineFormStep6 } from './create-shrine-form-step6';
 
 class CreateShrineFormContainer extends Component { 
    state = {
@@ -23,9 +27,44 @@ class CreateShrineFormContainer extends Component {
                />
             );
             break;
+
          case 2:
             return (
-               <CreateShrineFormStep2 />
+               <CreateShrineFormStep2 
+                  handleNextStep={ this._handleNextStep }
+               />
+            );
+            break;
+
+         case 3:
+            return (
+               <CreateShrineFormStep3
+                  handleNextStep={ this._handleNextStep }
+               />
+            );
+            break;
+
+         case 4:
+            return (
+               <CreateShrineFormStep4
+                  handleNextStep={ this._handleNextStep }
+               />
+            );
+            break;
+
+         case 5:
+            return (
+               <CreateShrineFormStep5
+                  handleNextStep={ this._handleNextStep }
+               />
+            );
+            break;
+
+         case 6:
+            return (
+               <CreateShrineFormStep6
+                  handleNextStep={ this._handleNextStep }
+               />
             );
             break;
       }
