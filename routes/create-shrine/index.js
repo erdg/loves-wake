2a15dc7a0cba8bd8a9fa6f92acd581657745556d
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-20 21:20:39 -0600

    ADD 'Avatar Rail', extra 'div's for form margins

--- routes/create-shrine/index.js
+++ routes/create-shrine/index.js
@@ -2,14 +2,21 @@ import { h, Component } from 'preact';
 
 import { FlexContainer } from '../../components/flex-container';
 import { CreateShrineFormContainer } from './components/create-shrine-form-container';
+import { AvatarRail } from './components/avatar-rail';
 
 class CreateShrine extends Component {
    render (props) {
       return (
          <FlexContainer 
+            avatarRail={
+               <div class="m-2">
+                  <AvatarRail />
+               </div>
+            }
             formRail={ 
-               <CreateShrineFormContainer 
-               /> 
+               <div class="m-2">
+                  <CreateShrineFormContainer /> 
+               </div>
             } 
          />
       );
