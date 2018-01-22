7f48ba7e3ba504ab017a7d19a76da7e2af0df4b9
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-22 15:22:23 -0600

    REFACTOR - lift state up

--- routes/create-shrine/index.js
+++ routes/create-shrine/index.js
@@ -1,11 +1,21 @@
 import { h, Component } from 'preact';
 
 import { FlexContainer } from '../../components/flex-container';
-import { CreateShrineFormContainer } from './components/create-shrine-form-container';
-import { AvatarRail } from './components/avatar-rail';
+import { CreateShrineFormContainer } from './create-shrine-form-container/';
+import { AvatarRail } from './avatar-rail/';
 
 class CreateShrine extends Component {
+   state = {
+      step: 1
+   };
+
+   _handleNextStep = () => {
+      let next = this.state.step + 1;
+      this.setState({ step: next });
+   }
+
    render (props) {
+      const step = this.state.step;
       return (
          <FlexContainer 
             avatarRail={
@@ -13,11 +23,15 @@ class CreateShrine extends Component {
                   <AvatarRail />
                </div>
             }
-            formRail={ 
+            formRail={
                <div class="m-2">
-                  <CreateShrineFormContainer /> 
+                  <h1>Create a New Shrine</h1>
+                  <CreateShrineFormContainer
+                     step={ this.state.step }
+                     handleNextStep={ this._handleNextStep }
+                  />
                </div>
-            } 
+            }
          />
       );
    }
