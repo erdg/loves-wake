338790712070dfe4f26d0d6dbbba86a546aa498f
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-24 16:18:51 -0600

    First draft of email list
    Allows to enter an arbitrary number of email addresses. Addresses can be
    removed from the list by clicking the 'x'. Not wired to the backend. No
    email address validation. Much to be done.

--- routes/create-shrine/create-shrine-form-step6/index.js
+++ routes/create-shrine/create-shrine-form-step6/index.js
@@ -1,20 +1,84 @@
 import { h, Component } from 'preact';
 
 import { TextInput } from '../../../components/form-inputs/';
+import { EmailChip } from '../../../components/chips';
 import { PrevStepButton } from '../prev-step-button';
 
-const CreateShrineFormStep6 = (props) => {
-   return (
-      <div>
 
-         <div class="row my-2">
-            <PrevStepButton
-               onClick={ props.handlePrevStep }
-            />
+class CreateShrineFormStep6 extends Component {
+   state = {
+      email: '',
+      emails: []
+   }
+
+   _handleEmailChange = (e) => {
+      this.setState({ email: e.target.value });
+   }
+
+   _addEmail = () => {
+      this.setState({ 
+         emails: [ ...this.state.emails, this.state.email ],
+         email: ''
+      });
+   }
+
+   _removeEmail = (e) => {
+      // save all emails except the one we want to get rid of...
+      // NOTE - originally intended to use e.target.value, but that isn't
+      // a thing when clicking on <a> tags. went with e.target.name as
+      // that was something I could set when the element is created.
+      // see line 67 below.
+      let emails = this.state.emails.filter( email => email !== e.target.name );
+      // and set the state to that.
+      this.setState({ emails: emails });
+   }
+
+   render (props, state) {
+      return (
+         <div>
+
+            <label class="form-label">Enter email addresses</label>
+            <div class="input-group">
+               <input 
+                  type="email" 
+                  class="form-input" 
+                  value={ this.state.email }
+                  onChange={ this._handleEmailChange }
+               />
+               <button 
+                  class="btn btn-primary input-group-btn"
+                  onClick={ this._addEmail }
+               >
+                  Add
+               </button>
+            </div>
+
+            <div class="mt-2" >
+               { this.state.emails.map((email) => (
+                  <span 
+                     class="chip"
+                  >
+                     { email }
+                     <a 
+                        class="btn btn-clear" 
+                        aria-label="Close" 
+                        role="button" 
+                        onClick={ this._removeEmail }
+                        name={ email }
+                     />
+                  </span>
+               )) }
+            </div>
+
+            <div class="row my-2">
+               <PrevStepButton
+                  onClick={ props.handlePrevStep }
+               />
+            </div>
+
          </div>
-
-      </div>
-   );
+      );
+   }
 }
 
 export { CreateShrineFormStep6 };  
