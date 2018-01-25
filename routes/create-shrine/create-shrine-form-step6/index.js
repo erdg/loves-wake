c789ef937629b88682ed1a03ffb39399f3c7b37a
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-25 16:17:43 -0600

    Add email validation

--- routes/create-shrine/create-shrine-form-step6/index.js
+++ routes/create-shrine/create-shrine-form-step6/index.js
@@ -1,4 +1,6 @@
 import { h, Component } from 'preact';
+import isEmail from 'validator/lib/isEmail';
+import classNames from 'classnames';
 
 import { TextInput } from '../../../components/form-inputs/';
 import { EmailChip } from '../../../components/chips';
@@ -8,7 +10,8 @@ import { PrevStepButton } from '../prev-step-button';
 class CreateShrineFormStep6 extends Component {
    state = {
       email: '',
-      emails: []
+      emails: [],
+      emailError: false
    }
 
    _handleEmailChange = (e) => {
@@ -16,6 +19,15 @@ class CreateShrineFormStep6 extends Component {
    }
 
    _addEmail = () => {
+      // if not valid email address
+      if ( !(isEmail(this.state.email)) ) {
+         // throw email error, don't submit
+         this.setState({ emailError: true });
+         return
+      } else {
+         this.setState({ emailError: false });
+      }
+
       this.setState({ 
          emails: [ ...this.state.emails, this.state.email ],
          email: ''
@@ -34,23 +46,44 @@ class CreateShrineFormStep6 extends Component {
    }
 
    render (props, state) {
+
+      let formClasses = classNames(
+         'form-group', { 'has-error': this.state.emailError }
+      );
+
+      let emailHintClasses = classNames(
+         'form-input-hint', { 'd-hide': !this.state.emailError }
+      );
+
       return (
          <div>
 
-            <label class="form-label">Enter email addresses</label>
-            <div class="input-group">
-               <input 
-                  type="email" 
-                  class="form-input" 
-                  value={ this.state.email }
-                  onChange={ this._handleEmailChange }
-               />
-               <button 
-                  class="btn btn-primary input-group-btn"
-                  onClick={ this._addEmail }
-               >
-                  Add
-               </button>
+            <div class={ formClasses } >
+
+               <label class="form-label">Enter email addresses</label>
+
+               <div class="input-group">
+
+                  <input 
+                     type="email" 
+                     class="form-input" 
+                     value={ this.state.email }
+                     onChange={ this._handleEmailChange }
+                  />
+
+                  <button 
+                     class="btn btn-primary input-group-btn"
+                     onClick={ this._addEmail }
+                  >
+                     Add
+                  </button>
+
+               </div>
+
+               <p class={ emailHintClasses }>
+                  Please enter a valid email address
+               </p>
+
             </div>
 
             <div class="mt-2" >
