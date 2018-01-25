10c7252b194e0e0c163f5d2d54b11fb68a99c2b8
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-01-25 16:58:15 -0600

    Add basic error checking
    Make sure a first and last name have been provided before moving to the
    next step. If not, show an error message and stay on the current step.

--- routes/create-shrine/create-shrine-form-step1/index.js
+++ routes/create-shrine/create-shrine-form-step1/index.js
@@ -1,28 +1,85 @@
 import { h, Component } from 'preact';
+import classNames from 'classnames';
 
 import { TextInput } from '../../../components/form-inputs/';
 import { NextStepButton } from '../next-step-button';
 
-const CreateShrineFormStep1 = (props) => {
-   return (
-      <div>
-         <TextInput 
-            label="First Name" 
-            firstName={ props.firstName }
-            onChange={ props.handleFirstNameChange }
-         />
-         <TextInput 
-            label="Last Name" 
-            lastName={ props.lastName }
-            onChange={ props.handleLastNameChange }
-         />
-         <div class="row my-2">
-            <NextStepButton 
-               onClick={ props.handleNextStep }
+class CreateShrineFormStep1 extends Component {
+   state = {
+      firstNameError: false,
+      lastNameError: false
+   }
+
+   _handleNextStep = () => {
+      if (!(this.props.firstName === '')) {
+         this.setState({ firstNameError: false});
+      } else {
+         this.setState({ firstNameError: true });
+      } 
+      
+      if (!(this.props.lastName === '')) {
+         this.setState({ lastNameError: false});
+      } else {
+         this.setState({ lastNameError: true });
+      } 
+      
+      if (this.state.firstNameError || this.state.lastNameError) {
+         return
+      }
+
+      this.props.handleNextStep();
+   }
+
+
+   render (props, state) {
+
+      let formClasses = classNames(
+         'form-group', { 'has-error': this.state.firstNameError || this.state.lastNameError }
+      );
+
+      let firstNameHintClasses = classNames(
+         'form-input-hint', { 'd-hide': !this.state.firstNameError }
+      );
+
+      let lastNameHintClasses = classNames(
+         'form-input-hint', { 'd-hide': !this.state.lastNameError }
+      );
+
+      return (
+
+         <div class={ formClasses }>
+
+            <TextInput 
+               label="First Name" 
+               firstName={ props.firstName }
+               onChange={ props.handleFirstNameChange }
             />
+
+            <p class={ firstNameHintClasses }>
+               Please enter a first name
+            </p>
+
+            <TextInput 
+               label="Last Name" 
+               lastName={ props.lastName }
+               onChange={ props.handleLastNameChange }
+            />
+
+            <p class={ lastNameHintClasses }>
+               Please enter a last name
+            </p>
+
+            <div class="row my-2">
+
+               <NextStepButton 
+                  onClick={ this._handleNextStep }
+               />
+
+            </div>
+
          </div>
-      </div>
-   );
+      );
+   }
 }
 
 export { CreateShrineFormStep1 };  
